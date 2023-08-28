d4ab6fee9be3cac988db2a5469ca2da3ca0e23a9
Hacker <Hacker@MacBook-Pro-2.local>
2023-08-28 21:41:11 +0600

    add movie functions

--- apps/movies/urls.py
+++ apps/movies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-
+from .views import CreateMovieView, MovieListView, MovieDetailView, DeleteMovieView
 
 urlpatterns = [
-
+    path('', MovieListView.as_view()), # api/movies/
+    path('create/', CreateMovieView.as_view()), # api/movies/create/
+    path('<int:pk>/', MovieDetailView.as_view()), # api/movies/1/
+    path('<int:pk>/delete/', DeleteMovieView.as_view()), # api/movies/1/delete/
 ]
--- apps/movies/views.py
+++ apps/movies/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from .serializers import MovieSerializer
+from .models import Movie
+from rest_framework import generics
+from apps.cinemas.permissions import IsAdminOrReadOnly
 
-# Create your views here.
+
+class MovieListView(generics.ListCreateAPIView):
+    """
+    List all movies, or create a new movie.
+    
+    Use this endpoint to retrieve a list of all existing movies or create a new movie.
+    """
+    serializer_class = MovieSerializer
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(is_there_at_the_box_office=True)
+        return queryset
+
+
+class MovieDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a movie instance.
+    
+    Use this endpoint to retrieve, update or delete a movie instance.
+    
+    Parameters:
+    - `pk` (int): Id of the movie.
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+
+class CreateMovieView(generics.CreateAPIView):
+    """
+    Create a new movie.
+    
+    Use this endpoint to create a new movie.
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+# Withdraw from the box office
+class WithdrawMovieView(generics.UpdateAPIView):
+    """
+    Withdraw a movie from the box office.
+    
+    Use this endpoint to withdraw a movie from the box office.
+    
+    Parameters:
+    - `pk` (int): Id of the movie.
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+
+class DeleteMovieView(generics.DestroyAPIView):
+    """
+    Delete a movie.
+    
+    Use this endpoint to delete a movie.
+    
+    Parameters:
+    - `pk` (int): Id of the movie.
+    """
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
